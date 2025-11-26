bb8ceb761f43d2832da26450adbdfe291c8a623b
Benedict Colin Yuwono <colin2laguna@gmail.com>
2025-11-26 20:02:48 +0000

    code overhaul

--- webots/controllers/ss_controller/ss_controller.py
+++ webots/controllers/ss_controller/ss_controller.py
@@ -2,10 +2,8 @@ from controller import Robot
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
-
 name = robot.getName()
 
-# --- Get the four wheel motors (names from your console) ---
 fr = robot.getDevice('front_right_wheel_joint')
 fl = robot.getDevice('front_left_wheel_joint')
 br = robot.getDevice('back_right_wheel_joint')
@@ -14,12 +12,10 @@ bl = robot.getDevice('back_left_wheel_joint')
 motors = [fr, fl, br, bl]
 
 for m in motors:
-    m.setPosition(float('inf'))          # velocity control mode
+    m.setPosition(float('inf'))
 
-# Try a safe speed first (you can tune this)
-BASE_SPEED = 15.0   # rad/s, usually <= max vel (~10-ish)
+BASE_SPEED = 15.0
 
 while robot.step(timestep) != -1:
-    # Simple forward motion: same velocity on all wheels
     for m in motors:
         m.setVelocity(BASE_SPEED)
